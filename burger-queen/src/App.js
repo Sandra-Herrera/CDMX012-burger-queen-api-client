e317acec53697d82c6dd49a006ac49a7125b57e3
Melissa Bracamonte <melissabracamonte30@gmail.com>
2022-06-10 10:23:20 -0500

    lista de empleados renderizada

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { UserContext } from "./database/UserProvider";
 import Login from "./components/login/Login";
 import Kitchen from "./components/kitchen/Kitchen";
+import Team from "./components/team/Team";
 import Signup from "./components/signUp/Signup";
 import Home from "./components/home/Home";
 import "./App.css";
@@ -22,33 +23,35 @@ function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />;
-      {userRole === "cocina" && (
+      {userRole?.toLowerCase() === "Kitchen".toLowerCase() && (
         <Route exact path="/kitchen" element={<Kitchen />} />
       )}
       ;
-      {userRole === "administrador" && (
+      {userRole?.toLowerCase() === "Administrator".toLowerCase() && (
         <Route exact path="/kitchen" element={<Kitchen />} />
       )}
+      ;{user && <Route exact path="/home" element={<Home />} />};
+      {userRole?.toLowerCase() === "Administrator".toLowerCase() && (
+        <Route exact path="/team" element={<Team />} />
+      )}
       ;
-      {user && <Route exact path="/home" element={<Home />} />}
-
-      {userRole === "administrador" && (
+      {userRole?.toLowerCase() === "Administrator".toLowerCase() && (
         <Route exact path="/signup" element={<Signup />} />
       )}
       ;
-      {userRole === "administrador" && (
+      {userRole?.toLowerCase() === "Administrator".toLowerCase() && (
         <Route exact path="/products" element={<Products />} />
       )}
       ;
-      {userRole === "mesero" && (
+      {userRole?.toLowerCase() === "Waiter".toLowerCase() && (
         <Route exact path="/order" element={<ViewMenuNewOrder />} />
       )}
       ;
-      {userRole === "administrador" && (
+      {userRole?.toLowerCase() === "Administrator".toLowerCase() && (
         <Route exact path="/order" element={<ViewMenuNewOrder />} />
       )}
       ;
-      {userRole === "administrador" && (
+      {userRole?.toLowerCase() === "Administrator".toLowerCase() && (
         <Route exact path="/NewOrder" element={<NewOrder />} />
       )}
       ;{!user && <Route path="*" element={<ErrorWithoutUser />} />};
