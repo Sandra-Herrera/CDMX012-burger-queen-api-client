9d9d3a8a63e07d96b9d07191b47158e394babefe
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-06-27 20:36:03 -0500

    prepare to use gh-pages

--- burger-queen/src/index.js
+++ burger-queen/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import UserProvider from './database/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserProvider>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </UserProvider>
 );
